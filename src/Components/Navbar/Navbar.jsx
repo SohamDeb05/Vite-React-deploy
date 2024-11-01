d2634e016f9fe38ae3a2bf748a88e991693ded79
Soham Deb <sohaam.deb@gmail.com>
2024-11-01 15:20:19 +0530

    To make it responsive

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -7,11 +7,12 @@ const Navbar = () => {
 
 const [menu,setMenu] = useState("home")
 
+
+
   return (
     <div className='navbar'> 
     <img src={footerlogo} alt="" />
     <ul  className="nav-menu">
-      
         <li><AnchorLink className='anchor-link'href='#home'><p onDoubleClick={()=>setMenu("home")}>Home</p></AnchorLink></li>
         <li><AnchorLink className='anchor-link'offset={50}href='#about'><p onDoubleClick={()=>setMenu("about me")}>About me</p></AnchorLink> </li>
         <li><AnchorLink className='anchor-link'offset={50}href='#contact'><p onDoubleClick={()=>setMenu("contact")}>Contact</p></AnchorLink></li>
